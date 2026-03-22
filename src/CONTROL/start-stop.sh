7817d1ae0f1384e5f79d94a9f46aac414732d23d
007revad <39733752+007revad@users.noreply.github.com>
2026-03-23 07:22:18 +1100

    v1.0.1

--- src/CONTROL/start-stop.sh
+++ src/CONTROL/start-stop.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 NAME="Asustor Ookla Speedtest"
+PKG_DIR="/usr/local/AppCentral/Asustorspeedtest"
+PIDFILE="${PKG_DIR}/var/httpd.pid"
 
 case "$1" in
     start)
         echo "Starting $NAME"
+        # BusyBox httpd with CGI support
+        busybox httpd -p 39876 -h "${PKG_DIR}/webman" \
+            -c "${PKG_DIR}/var/httpd.conf" \
+            -f &
+        echo $! > "$PIDFILE"
         ;;
     stop)
         echo "Stopping $NAME"
-        ;;
-    reload)
-        echo "Reloading $NAME"
+        if [ -f "$PIDFILE" ]; then
+            kill $(cat "$PIDFILE") 2>/dev/null
+            rm -f "$PIDFILE"
+        fi
         ;;
     restart|force-reload)
-        echo "Restarting $NAME"
+        $0 stop
+        sleep 1
+        $0 start
         ;;
     *)
-        echo "Usage: $0 {start|stop|reload|force-reload|restart}"
+        echo "Usage: $0 {start|stop|restart}"
         exit 2
         ;;
 esac
-
 exit 0
